acc4c8bea7071fa7336540a9bc663c4bd06bcadb
Mohammed Refaeyeh <157818062+MoRefaeyeh@users.noreply.github.com>
2026-05-20 15:06:57 +0000

    Add initial Django project structure and configuration files

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,75 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from djongo import models
+
+from django.db import connection
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    user = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    user = models.CharField(max_length=100)
+    points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        # Delete all data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User.objects.create_user(username='ironman', email='ironman@marvel.com', password='pass', first_name='Tony', last_name='Stark'),
+            User.objects.create_user(username='spiderman', email='spiderman@marvel.com', password='pass', first_name='Peter', last_name='Parker'),
+            User.objects.create_user(username='batman', email='batman@dc.com', password='pass', first_name='Bruce', last_name='Wayne'),
+            User.objects.create_user(username='superman', email='superman@dc.com', password='pass', first_name='Clark', last_name='Kent'),
+        ]
+
+        # Create activities
+        Activity.objects.create(user='ironman', type='Running', duration=30)
+        Activity.objects.create(user='spiderman', type='Cycling', duration=45)
+        Activity.objects.create(user='batman', type='Swimming', duration=60)
+        Activity.objects.create(user='superman', type='Flying', duration=120)
+
+        # Create leaderboard
+        Leaderboard.objects.create(user='ironman', points=100)
+        Leaderboard.objects.create(user='spiderman', points=80)
+        Leaderboard.objects.create(user='batman', points=90)
+        Leaderboard.objects.create(user='superman', points=110)
+
+        # Create workouts
+        Workout.objects.create(name='Super Strength', description='Lift heavy objects')
+        Workout.objects.create(name='Web Swing', description='Swing between buildings')
+        Workout.objects.create(name='Stealth', description='Move unseen')
+        Workout.objects.create(name='Flight', description='Fly through the sky')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
+
+        # Unique index on email must be created using mongosh, not Django ORM.
